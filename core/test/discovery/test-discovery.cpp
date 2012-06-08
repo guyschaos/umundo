8054d1ac1b1711e81fe9befd8646846b9334ec8e
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2012-06-08 13:15:15 +0200

    Added more strict test for waitForPublishers

--- core/test/discovery/test-discovery.cpp
+++ core/test/discovery/test-discovery.cpp
@@ -32,36 +32,67 @@ public:
 };
 
 int main(int argc, char** argv, char** envp) {
+  setenv("UMUNDO_LOGLEVEL", "4", 1);
+
 	TestDiscoverer* testDiscoverer = new TestDiscoverer();
 	shared_ptr<NodeQuery> query = shared_ptr<NodeQuery>(new NodeQuery("fooDomain", testDiscoverer));
 	Discovery::browse(query);
+
 	TestDiscoverable* testDiscoverable = new TestDiscoverable("fooDomain");
 	Discovery::add(testDiscoverable);
 	while(receives < 1)
 		UMUNDO_WAIT(monitor);
   
-  setenv("UMUNDO_LOGLEVEL_DISC", "1", 1);
-  
   // test node / publisher / subscriber churn
-  for (int i = 0; i < 2; i++) {  
+  for (int i = 0; i < 10; i++) {  
     Node* node1 = new Node();
     Node* node2 = new Node();
     for (int j = 0; j < 2; j++) {
-      Subscriber* sub = new Subscriber("foo", NULL);
+      Subscriber* sub1 = new Subscriber("foo", NULL);
+      Subscriber* sub2 = new Subscriber("foo", NULL);
+      Subscriber* sub3 = new Subscriber("foo", NULL);
       Publisher* pub = new Publisher("foo");
 
+      int subs = 0;
       node1->addPublisher(pub);
-      node2->addSubscriber(sub);
-      
-			Thread::sleepMs(10);
-      int subs = pub->waitForSubscribers(1);
-      if (subs != 1)
-        abort();
-      
-      node2->removeSubscriber(sub);
+      subs = pub->waitForSubscribers(0);
+      assert(subs == 0);
+
+      node2->addSubscriber(sub1);      
+      subs = pub->waitForSubscribers(1);
+      assert(subs == 1);
+
+      node2->addSubscriber(sub2);
+      subs = pub->waitForSubscribers(2);
+      assert(subs == 2);
+
+      node2->addSubscriber(sub3);
+      subs = pub->waitForSubscribers(3);
+      assert(subs == 3);
+
+      node2->removeSubscriber(sub1);
+      Thread::sleepMs(50);
+      subs = pub->waitForSubscribers(0);
+      assert(subs == 2);
+
+      node2->removeSubscriber(sub2);
+      Thread::sleepMs(50);
+      subs = pub->waitForSubscribers(0);
+      assert(subs == 1);
+
+      node2->removeSubscriber(sub3);
+      Thread::sleepMs(50);
+      subs = pub->waitForSubscribers(0);
+      assert(subs == 0);
+
+      node2->removeSubscriber(sub1);
+      node2->removeSubscriber(sub2);
+      node2->removeSubscriber(sub3);
       node1->removePublisher(pub);
       
-      delete sub;
+      delete sub1;
+      delete sub2;
+      delete sub3;
       delete pub;
     }
     delete node1;
